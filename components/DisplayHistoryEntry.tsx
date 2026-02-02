2a76f388922822bea83c064c077806d7f5898064
David A J Lee <dave@davelee.de>
2026-02-02 22:16:34 +0100

    Use the timezone to display times instead of utc

--- components/DisplayHistoryEntry.tsx
+++ components/DisplayHistoryEntry.tsx
@@ -5,6 +5,7 @@ import { Alert, StyleSheet, Text, View } from "react-native";
 import IconButton from "./IconButton";
 import { deleteHistoryEntry } from "../utilities/sqlite";
 import { HistoryEntryResult } from "../models/HistoryEntryResult";
+import { getCurrencies, getLocales, getTimeZone } from "react-native-localize";
 
 type DisplayHistoryEntryProps = {
     entries: HistoryEntryResult[];
@@ -25,19 +26,39 @@ export function DisplayHistoryEntry({entries, id, sum, datetime, categoryName, c
             return { backgroundColor: categoryColour };
         }
     }
+
+    function getCurrency(value: string) {
+        return Intl.NumberFormat(getLocales()[0].languageTag, { style: "currency", currency: getCurrencies()[0] }).format( parseFloat(value) )
+    }
+
+    function convertUTCDateToLocal(datetime: string) {
+        let timezone = getTimeZone();
+        console.log('Timezone is ' + timezone);
+        let date = new Date(datetime);
+        let options: Intl.DateTimeFormatOptions = {
+            timeZone: timezone,
+            hour12: false,
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit'
+        };
+        return new Intl.DateTimeFormat(getLocales()[0].languageTag, options).format(date);
+    }
     
     return (
         <View style={styles.container}>
             <View style={styles.leftRow}>
-                <Text style={styles.leftRowText}>{datetime.split("T")[0]}</Text>
-                <Text style={styles.leftRowText}>{datetime.split("T")[1].split(".")[0]}</Text>
+                <Text style={styles.leftRowText}>{convertUTCDateToLocal(datetime)}</Text>
             </View>
             <View style={styles.middleRow}>
                 {categoryName ? <Text style={[styles.listText, getBackgroundColour()]}>{categoryName}</Text> : <Text style={[styles.listText, styles.unassigned]}>Unassigned</Text>}
                 <Text style={styles.listText}>{description}</Text>
             </View>
             <View style={styles.rightRow}>
-                <Text style={styles.rightRowText}>€{parseFloat(sum).toFixed(2)}</Text>
+                <Text style={styles.rightRowText}>{getCurrency(parseFloat(sum).toFixed(2))}</Text>
                 <IconButton color="white" onPress={() => { 
                     Alert.alert('Confirm deletion', 'Are you sure you want to delete this entry?', [
                         {
