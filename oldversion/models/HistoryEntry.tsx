2133518229e9b108bc842cb28d2a0a2b4bcddeec
David A J Lee <dave@davelee.de>
2026-08-29 13:05:07 +0200

    Remove old version from branch which is no longer required

--- oldversion/models/HistoryEntry.tsx
+++
@@ -1,7 +0,0 @@
-export type HistoryEntry = {
-    id: number;
-    sum: string;
-    description: string;
-    categoryName: string;
-    datetime: string;
-}
--- oldversion/models/HistoryEntryResult.tsx
+++
@@ -1,9 +0,0 @@
-export type HistoryEntryResult = {
-    id: number;
-    sum: string;
-    description: string;
-    categoryName: string;
-    categoryColour: string;
-    datetime: string;
-    type: string;
-}
